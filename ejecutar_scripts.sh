690fffa3645e8b7337c91be0a92457352fbef9ed
sukigsx <sukigsx.mbsistemas@gmail.com>
2024-03-09 22:18:10 +0100

    Update ejecutar_scripts.sh

--- ejecutar_scripts.sh
+++ ejecutar_scripts.sh
@@ -62,7 +62,7 @@ fi
 software_necesario(){
 var_software="no"
 echo -e " Verificando software necesario:"
-software="which git diff ping figlet -c apt fzf curl jq" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+software="which git diff ping figlet apt fzf curl jq" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
 for paquete in $software
 do
 which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
