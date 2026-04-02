8c555d7b2664fd62f9e5ff857b97e4ee7b5ff363
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-04-02 10:25:39 +0200

    Update Ejecutar_scripts.sh

--- Ejecutar_scripts.sh
+++ Ejecutar_scripts.sh
@@ -161,6 +161,7 @@ for comando in "${!requeridos[@]}"; do
                 echo ""
                 echo -e " ${rojo}No se puede ejecutar el script sin todo el software necesario.${borra_colores}"
                 echo ""
+                read p
                 exit 1
             else
                 echo -e "${amarillo} Se necesita instalar ${borra_colores}$comando${amarillo} para la ejecucion del script${borra_colores}"
