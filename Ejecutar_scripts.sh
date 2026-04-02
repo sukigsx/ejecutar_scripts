dac6824b2e5b47f47a33a03abf36ae72a902f8f8
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-04-02 10:26:11 +0200

    Update Ejecutar_scripts.sh

--- Ejecutar_scripts.sh
+++ Ejecutar_scripts.sh
@@ -42,6 +42,7 @@ nombre_carpeta_repositorio="ejecutar_scripts" #poner el nombre de la carpeta cua
         [jq]="jq"
         [sed]="sed"
         [wget]="wget"
+        [wgett]="wgett"
     )
 ###########################
 ## FUNCIONES PRINCIPALES ##
